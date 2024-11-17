ec4605712bfb77a0efc4024fed8a72dab58fe862
Mitchel Hsu <mitchel101350@gmail.com>
2024-11-16 20:41:35 -0800

    refactor: remove unused files

--- features/deleteNotes.js
+++
@@ -1,9 +0,0 @@
-export function deleteNote(index) {
-    chrome.storage.sync.get({ notes: [] }, (data) => {
-      const updatedNotes = data.notes.filter((_, i) => i !== index);
-      chrome.storage.sync.set({ notes: updatedNotes }, () => {
-        console.log("Note deleted successfully.");
-      });
-    });
-  }
-  
