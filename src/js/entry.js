309960a1d1b080bec52ac80c932587c1c133ad31
Rashi Bhansali <rashibhansali2000@gmail.com>
2024-11-24 19:25:48 -0800

    feat: added cell-like structure to notes

--- src/js/entry.js
+++ src/js/entry.js
@@ -18,59 +18,58 @@ class NotesApp {
   }
 
   async loadNotes() {
-    const url = await this.getUrl();
-    const note =
-      (await this.noteRepository.getNoteByUrl(url)) ||
-      (await this.noteRepository.addNote(url));
-    console.log('Inside load notes: ', note);
-    await this.notesView.render(note);
+    try{
+      const url = await this.getUrl();
+      const note = await this.noteRepository.getNoteByUrl(url) || await this.noteRepository.addNote(url);
+      console.log("Inside load notes: ", note);
+      await this.notesView.render(note);
+    }
+    catch (error) {
+      console.error('Error in loading notes', error);
+    }
   }
 
   setupEventListeners() {
-    this.notesView.setOnDeleteCell(
-      async (timestamp) => await this.handleDeleteCell(timestamp),
+    this.notesView.setOnDeleteCell(async (timestamp) =>
+      await this.handleDeleteCell(timestamp),
     );
-    this.notesView.setOnAddCell(
-      async (timestamp, content, cellType, targetTimestamp) =>
-        await this.handleAddCell(timestamp, content, cellType, targetTimestamp),
+    this.notesView.setOnAddCell(async (timestamp, content, cellType, targetTimestamp) =>
+      await this.handleAddCell(timestamp, content, cellType, targetTimestamp),
     );
-    this.notesView.setOnUpdateCell(
-      async (timestamp, content, cellType) =>
-        await this.handleUpdateCell(timestamp, content, cellType),
+    this.notesView.setOnUpdateCell(async (timestamp, content, cellType) =>
+      await this.handleUpdateCell(timestamp, content, cellType),
     );
   }
 
   async handleAddCell(timestamp, content, cellType, targetTimestamp) {
-    await this.noteRepository.addCellToNote(
-      await this.getUrl(),
-      timestamp,
-      content,
-      cellType,
-      targetTimestamp,
-    );
+    try{
+      await this.noteRepository.addCellToNote(await this.getUrl(), timestamp, content, cellType, targetTimestamp);
+    }
+    catch (error) {
+      console.error('Error in adding new cell to the note', error);
+    }
   }
 
   async handleDeleteCell(timestamp) {
-    await this.noteRepository.deleteCellFromNote(
-      await this.getUrl(),
-      timestamp,
-    );
+    try{
+      await this.noteRepository.deleteCellFromNote(await this.getUrl(), timestamp);
+    }
+    catch (error) {
+      console.error('Error in deleting cell from the note', error);
+    }
   }
 
   async handleUpdateCell(timestamp, content, cellType) {
-    await this.noteRepository.updateCellContent(
-      await this.getUrl(),
-      timestamp,
-      content,
-      cellType,
-    );
+    try{
+      await this.noteRepository.updateCellContent(await this.getUrl(), timestamp, content, cellType);
+    }
+    catch (error) {
+      console.error('Error in saving cell content to the note', error);
+    }
   }
 
   async getUrl() {
-    const [tab] = await chrome.tabs.query({
-      active: true,
-      currentWindow: true,
-    });
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     return tab.url;
   }
 }
--- src/js/repositories/NoteRepository.js
+++ src/js/repositories/NoteRepository.js
@@ -1,11 +1,11 @@
-import { StorageService } from '../services/StorageService.js';
+import { StorageService } from '../services/StorageService.js'
 import { Note, NoteCell } from '../models/Note.js';
 
 // This is layer between our app's business logic and the data storage layer
 export class NoteRepository {
   constructor() {
     this.storageService = new StorageService();
-    this.STORAGE_KEY = 'notes';
+    static.STORAGE_KEY = 'notes';
   }
 
   async getAllNotes() {
@@ -49,21 +49,61 @@ export class NoteRepository {
     }
   }
 
+  async getNoteByUrl(url) {
+    const notes = await this.getAllNotes();
+    return notes.find((note) => note.url === url);
+  }
+
+  async addNote(url) {
+    try{
+      const notes = await this.getAllNotes();
+      const newNote = new Note(url);
+      await this.storageService.set(this.STORAGE_KEY, [...notes, newNote]);
+      return newNote;
+    }
+    catch (error) {
+      console.error('Failed to add note to storage:', error);
+      throw error;
+    }
+  }
+
+  async addCellToNote(url, timestamp, content, cellType, targetTimestamp) {
+    const notes = await this.getAllNotes();
+    const noteToUpdate = notes.find((note) => note.url === url) || this.addNote(url);
+    const newCell = new NoteCell(timestamp = timestamp, cellType = cellType, content = content);
+    if(noteToUpdate){
+      if (targetTimestamp) {
+        // Find the target cell index
+        const targetIndex = noteToUpdate.cells.findIndex(cell => cell.timestamp === targetTimestamp);
+        
+        if (targetIndex !== -1) {
+          // Insert the new cell after the target cell
+          noteToUpdate.cells.splice(targetIndex, 0, newCell);
+        }
+      } 
+      else {
+        // If no targetTimestamp is provided, add at the end of the cells array
+        noteToUpdate.cells.push(newCell);
+      }
+      await this.storageService.set(this.STORAGE_KEY, notes);
+    }
+  }
+
   async updateCellContent(url, timestamp, content, cellType) {
     const notes = await this.getAllNotes();
     const note = notes.find((note) => note.url === url);
-
+    
     if (!note || !note.cells) {
       console.error('Note not found while updating content.');
       return;
     }
-
+  
     // Find the cell by timestamp and update its content
-    const cell = note.cells.find((cell) => cell.timestamp === timestamp);
+    const cell = note.cells.find(cell => cell.timestamp === timestamp);
     if (cell) {
       cell.content = content;
       cell.cellType = cellType;
-
+  
       // Save the updated note to the database
       await this.storageService.set(this.STORAGE_KEY, notes);
       console.log('Updated cell content saved:', cell);
@@ -73,14 +113,17 @@ export class NoteRepository {
   }
 
   async deleteCellFromNote(url, cellTimestamp) {
-    const notes = await this.getAllNotes();
-    const noteToUpdate =
-      notes.find((note) => note.url === url) || this.addNote(url);
-    if (noteToUpdate) {
-      noteToUpdate.cells = noteToUpdate.cells.filter(
-        (cell) => cell.timestamp !== cellTimestamp,
-      );
-      await this.storageService.set(this.STORAGE_KEY, notes);
+    try{
+      const notes = await this.getAllNotes();
+      const noteToUpdate = notes.find((note) => note.url === url) || this.addNote(url);
+      if(noteToUpdate){
+        noteToUpdate.cells = noteToUpdate.cells.filter((cell) => cell.timestamp !== cellTimestamp);
+        await this.storageService.set(this.STORAGE_KEY, notes);
+      }
+    }
+    catch (error) {
+      console.error('Failed to delete note from storage:', error);
+      throw error;
     }
   }
 }
