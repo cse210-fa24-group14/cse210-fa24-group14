c607a4537534dc675ce3520016724858333a628a
Mitchel Hsu <mitchel101350@gmail.com>
2024-12-06 16:03:52 -0800

    feat: add initial performance tests

--- playwright.config.js
+++ playwright.config.js
@@ -15,11 +15,11 @@ module.exports = {
       },
     },
     {
-      name: 'e2e', // Project for E2E tests
-      testDir: './tests/e2e', // Points to the e2e folder
+      name: 'performance', // Project for performance tests
+      testDir: './tests/performance', // Points to the performance folder
       use: {
         browserName: 'chromium',
-        headless: false, // We need a visible browser for E2E tests
+        headless: true, // We need a visible browser for E2E tests
         args: [
           `--disable-extensions-except=${path.resolve(__dirname)}`,
           `--load-extension=${path.resolve(__dirname)}`,
