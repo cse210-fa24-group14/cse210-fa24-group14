6b70b80bf6ce0fef5e5abf6c1e626c242004e00a
Mitchel Hsu <40386247+MitchelHsu@users.noreply.github.com>
2024-12-09 19:01:41 -0800

    feat: add performance tests (#59)
    * feat: add initial performance tests
    
    * chore: fix e2e test
    
    * chore: fix performance tests and improve speed

--- playwright.config.js
+++ playwright.config.js
@@ -3,8 +3,8 @@ import path from 'path';
 module.exports = {
   projects: [
     {
-      name: 'unit', // Project for unit tests
-      testDir: './tests/unit', // Points to the tests folder
+      name: 'e2e', // Project for E2E tests
+      testDir: './tests/e2e', // Points to the e2e folder
       use: {
         browserName: 'chromium', // Can be configured to use chromium for unit tests
         headless: false,
@@ -15,8 +15,8 @@ module.exports = {
       },
     },
     {
-      name: 'e2e', // Project for E2E tests
-      testDir: './tests/e2e', // Points to the e2e folder
+      name: 'performance', // Project for performance tests
+      testDir: './tests/performance', // Points to the performance folder
       use: {
         browserName: 'chromium',
         headless: false, // We need a visible browser for E2E tests
