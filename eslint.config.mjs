255a2255bb4106cb8f95a95a3bb0147b4b728f52
Mitchel Hsu <40386247+MitchelHsu@users.noreply.github.com>
2024-12-10 16:03:50 -0800

    feat: add support for jsdocs (#61)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import prettierConfig from 'eslint-config-prettier';
 import pluginPrettier from 'eslint-plugin-prettier';
 import pluginHtml from 'eslint-plugin-html';
 import pluginImport from 'eslint-plugin-import';
+import jsdoc from 'eslint-plugin-jsdoc';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -33,9 +34,27 @@ export default [
       prettier: pluginPrettier,
       html: pluginHtml,
       import: pluginImport,
+      jsdoc: jsdoc,
     },
     rules: {
       'prettier/prettier': 'error',
+      'jsdoc/require-jsdoc': [
+        'warn',
+        {
+          require: {
+            FunctionDeclaration: true,
+            MethodDefinition: true,
+            ClassDeclaration: true,
+          },
+        },
+      ],
+      'jsdoc/require-param': 'warn',
+      'jsdoc/require-returns': 'warn',
+    },
+    settings: {
+      jsdoc: {
+        mode: 'jsdoc',
+      },
     },
   },
 ];
