6e83fd58e9ff6d0e32cb135ad6203ad70c5e4c59
Philemon Putra <pgputra@ucsd.edu>
2024-12-10 18:35:52 -0800

    Revert "feat: add support for jsdocs (#61)"
    This reverts commit 3676083e49ee3cc2b8000d2fd4223b2e7b8cb486.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,6 @@ import prettierConfig from 'eslint-config-prettier';
 import pluginPrettier from 'eslint-plugin-prettier';
 import pluginHtml from 'eslint-plugin-html';
 import pluginImport from 'eslint-plugin-import';
-import jsdoc from 'eslint-plugin-jsdoc';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -34,27 +33,9 @@ export default [
       prettier: pluginPrettier,
       html: pluginHtml,
       import: pluginImport,
-      jsdoc: jsdoc,
     },
     rules: {
       'prettier/prettier': 'error',
-      'jsdoc/require-jsdoc': [
-        'warn',
-        {
-          require: {
-            FunctionDeclaration: true,
-            MethodDefinition: true,
-            ClassDeclaration: true,
-          },
-        },
-      ],
-      'jsdoc/require-param': 'warn',
-      'jsdoc/require-returns': 'warn',
-    },
-    settings: {
-      jsdoc: {
-        mode: 'jsdoc',
-      },
     },
   },
 ];
